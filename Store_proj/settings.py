441eb90170b3e55c107e60bf67c226f589ed91d8
eladh23 <eladelad121212@gmail.com>
2023-11-27 21:46:00 +0200

    update setting

--- Store_proj/settings.py
+++ Store_proj/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'https://eladh23.github.io/React-Store/',
+    'https://eladh23.github.io/',
      "http://127.0.0.1:9000",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
